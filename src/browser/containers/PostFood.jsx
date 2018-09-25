46089b955c22d135d81c3e3f25054b603343dccc
Thomas Bui <thomasbui1997@gmail.com>
2018-09-25 19:32:56 -0400

    post form changes

--- src/browser/containers/PostFood.jsx
+++ src/browser/containers/PostFood.jsx
@@ -5,7 +5,20 @@ export default class PostFood extends React.Component{
   render(){
       return(
         <div>
-            Post Food
+            Post Location <br></br>
+
+            <form>
+              Location: <br></br>
+              <input type="text" name="location"></input>
+              <br></br>
+              Time: <br></br>
+              <input type="text" name="time"></input>
+              <br></br>
+              Description: <br></br>
+              <input type="text" name="description"></input>
+              <br></br>
+              <button>Submit</button>
+            </form>
         </div>
       );
   }
