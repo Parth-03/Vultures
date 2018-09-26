d176d6389596bd37054a93ad84bcabbf20c0913c
Thomas Bui <thomasbui1997@gmail.com>
2018-09-26 19:25:03 -0400

    Updated form page

--- src/browser/containers/PostFood.jsx
+++ src/browser/containers/PostFood.jsx
@@ -1,25 +1,36 @@
 import * as React from 'react';
 
+const space = {
+  margin: "50px"
+}
 
 export default class PostFood extends React.Component{
+
   render(){
       return(
-        <div>
-            Post Location <br></br>
-
-            <form>
-              Location: <br></br>
-              <input type="text" name="location"></input>
-              <br></br>
-              Time: <br></br>
-              <input type="text" name="time"></input>
-              <br></br>
-              Description: <br></br>
-              <input type="text" name="description"></input>
-              <br></br>
-              <button>Submit</button>
-            </form>
+        <center>
+        <div style={space}>
+            <h1>Post Location</h1><br></br>
+            <p>Use the form below to submit information about an event.</p>
+            <div style={space}>
+              <form>
+                  <div style={space}>
+                  Location: <br></br>
+                  <input type="text" name="location"></input>
+                  <br></br>
+                  Time: <br></br>
+                  <input type="text" name="time"></input>
+                  <br></br>
+                  Description: <br></br>
+                  <input type="text" name="description"></input>
+                  </div>
+                <br></br>
+                <button>Submit</button>
+              </form>
+            </div>
         </div>
+        </center>
+        
       );
   }
 }
