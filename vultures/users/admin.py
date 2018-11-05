ca84e2c50ba18cd9ce07ccf2bec42edd098c7e79
GarrisonQian <garrisonqian@gmail.com>
2018-11-05 18:21:40 -0500

    registered admin models

--- vultures/users/admin.py
+++ vultures/users/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Person
+from .models import Person, FoodComment, FoodPost
 
 
 @admin.register(Person)
 class PersonAdminModel(UserAdmin):
     pass
+
+@admin.register(FoodComment)
+class FoodCommentAdminModel(admin.ModelAdmin):
+	pass
+
+@admin.register(FoodPost)
+class FoodPostAdminModel(admin.ModelAdmin):
+	pass
--- vultures/users/models.py
+++ vultures/users/models.py
@@ -7,3 +7,52 @@ class Person(AbstractUser):
 
     def __str__(self):
         return "User(<{}>)".format(self.username)
+
+
+class FoodComment(models.Model):
+
+	commenter = models.ForeignKey("Person", on_delete=models.SET_NULL, null=True)
+
+	commentPost = models.ForeignKey("FoodPost", on_delete=models.SET_NULL, null=True)
+
+	commentContent = models.TextField(
+		max_length=1000, help_text="Enter comment"
+	)
+
+	commentScore = models.IntegerField()
+
+	commentDateTime = models.DateTimeField(null=True, blank=True)
+
+	class Meta:
+		ordering = ["commentDateTime"]
+
+	def __str__(self):
+		return self.commentContent
+
+	def display_author(self):
+		return self.commenter
+
+
+class FoodPost(models.Model):
+
+	postScore = models.IntegerField()
+
+	location = models.CharField(
+		max_length=1000, help_text="Enter coords for the food"
+	)
+
+	postUser = models.ForeignKey("Person", on_delete=models.SET_NULL, null=True
+		)
+
+	postDate = models.DateTimeField(null=True, blank=True)
+
+	postInfo = models.CharField(
+		max_length=1000, help_text="Enter info about the food",default="Food is here"
+	)
+
+	class Meta:
+		ordering = ["postDate"]
+
+	def __str__(self):
+		return self.postInfo
+
